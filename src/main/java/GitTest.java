ea9a41f502b2428d673053ef12a0980201ed0ec2
linjiale <lin15270414523@163.com>
2023-11-09 13:55:48 +0800

    second commit

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -12,5 +12,6 @@ package src.main.java;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
